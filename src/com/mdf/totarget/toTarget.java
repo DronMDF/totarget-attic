f092e40f687a87673f9dc7902355eb987939faba
Andrey Valyaev <dron@securitycode.ru>
2011-08-25 23:48:28 +0400

    Добавилось поле ввода, и обработчик ввода

--- src/com/mdf/totarget/toTarget.java
+++ src/com/mdf/totarget/toTarget.java
@@ -2,13 +2,45 @@ package com.mdf.totarget;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.view.View.OnKeyListener;
+import android.view.KeyEvent;
+import android.view.View;
 
 public class toTarget extends Activity
 {
+	private EditText inputView;
+
+	private final OnKeyListener keyListener = new OnKeyListener() {
+		public boolean onKey(View view, int keyCode, KeyEvent event) {
+			if (keyCode != KeyEvent.KEYCODE_ENTER) {
+				return false;
+			}
+
+			if (event.getAction() != KeyEvent.ACTION_DOWN) {
+				return false;
+			}
+
+			addTask();
+			return true;
+		}
+	};
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+
+		inputView = (EditText)findViewById(R.id.edittext);
+		inputView.setOnKeyListener(keyListener);
+	}
+
+	private void addTask()
+	{
+		String task = inputView.getText().toString();
+		Toast.makeText(toTarget.this, task, Toast.LENGTH_SHORT).show();
+		inputView.setText("");
 	}
 }
