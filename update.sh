eeac9a634b8e66a3b42f7cfef92be66f10629080
Andrey Valyaev <dron@securitycode.ru>
2011-08-30 22:41:37 +0400

    путь неправильный походу

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 DIR=`dirname $0`
 
 android update project --name toTarget --target 1 --path ${DIR}
-android update test-project --main ${DIR} --path ${DIR}/test
+android update test-project --main .. --path ${DIR}/test
